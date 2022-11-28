640bfef0f0f5ba602ffee8f7b8db86848ece5bfc
Grégoire Bézier <60408187+Herosbrine@users.noreply.github.com>
2022-11-28 17:21:59 +0100

    [ADD] | comment to my script

--- hackwifi.py
+++ hackwifi.py
@@ -16,32 +16,32 @@ class BCScan:
 
 
 class CollectSSID(BCScan):
-    def __init__(self) -> None:
+    def __init__(self) -> None: # Init the class
         super().__init__()
 
     def Translate(self, text, lang):
-        translator= Translator(to_lang=lang)
-        translation = translator.translate(text)
-        return translation
+        translator= Translator(to_lang=lang) # Translate the text to the language of the system
+        translation = translator.translate(text) # Translate the text to the language of the system
+        return translation # Return the translation
 
     def collect(self) -> Dict[Any, Any]:
-        if not platform.uname().system.lower() == "windows":
-            raise OSError("Requires the Windows operating system")
-        ssid_pw_map: Dict[Any, Any] = {}
+        if not platform.uname().system.lower() == "windows": # Check if the system is windows
+            raise OSError("Requires the Windows operating system") # Raise an error if the system is not windows
+        ssid_pw_map: Dict[Any, Any] = {} # Create a dictionary
         data = str(subprocess.check_output(
-            ["netsh", "wlan", "show", "profiles"]))
+            ["netsh", "wlan", "show", "profiles"])) # Get the output of the command
         data = data.replace("\\r", "")
         data = data.replace("\\n", "")
         data = data.split("\\xff")
         TranslateSentence = "Profil Tous les utilisateurs"
         TranslateSentence2 = "Contenu de la cl\\x82            : "
         TranslateSentence3 = "\\r\\n\\r\\nParam"
-        windll = ctypes.windll.kernel32
-        windll.GetUserDefaultUILanguage()
-        lang = locale.windows_locale[windll.GetUserDefaultUILanguage()].split("_")[0]
-        RealLang = Language.get(lang).display_name(lang)
+        windll = ctypes.windll.kernel32 # Get the language of the system
+        windll.GetUserDefaultUILanguage() # Get the language of the system
+        lang = locale.windows_locale[windll.GetUserDefaultUILanguage()].split("_")[0] # Get the language of the system, ex: fr, en, es, etc.
+        RealLang = Language.get(lang).display_name(lang) # Get the real name of the language, ex: fr = French, en = English, es = Spanish, etc.
         if (lang != "fr"):
-            TranslateSentence = self.Translate("Profil Tous les utilisateurs", RealLang)
+            TranslateSentence = self.Translate("Profil Tous les utilisateurs", RealLang) # Translate the sentence to the language of the system
             TranslateSentence2 = self.Translate("Contenu de la clée", RealLang)
             TranslateSentence2 += "            : "
             TranslateSentence3 = self.Translate("Paramètres", RealLang)
@@ -57,23 +57,23 @@ class CollectSSID(BCScan):
             profiles.append(line)
         for ssid in profiles:
             results = str(subprocess.check_output(
-                ["netsh", "wlan", "show", "profiles", ssid, "key=clear"]))
+                ["netsh", "wlan", "show", "profiles", ssid, "key=clear"])) # Get the output of the command
             results = results[results.find(
                 TranslateSentence2):]
             results = results.replace(
                 TranslateSentence2, "")
-            results = results[:results.find(TranslateSentence3)]
+            results = results[:results.find(TranslateSentence3)] # Get the password
             if (results):
-                ssid_pw_map.setdefault(ssid, results)
+                ssid_pw_map.setdefault(ssid, results) # Add the ssid and the password to the dictionary
             else:
-                print("No password found for SSID: {}".format(ssid))
+                print("No password found for SSID: {}".format(ssid)) # Print if there is no password
         for ssid, pw in ssid_pw_map.items():
-            print("SSID: {} | Password: {}".format(ssid, pw))
+            print("SSID: {} | Password: {}".format(ssid, pw)) # Print the ssid and the password
         return 0
 
 def main():
-    ssid_collector = CollectSSID()
-    ssid_collector.collect()
+    ssid_collector = CollectSSID() # Create an instance of the class
+    ssid_collector.collect() # Call the function
 
 
 if __name__ == "__main__":
